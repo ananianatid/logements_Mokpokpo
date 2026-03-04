4323d3b9af7d3ec6df1a689bff699d97a00de275
ananianatid <ananianatid@gmail.com>
2026-03-04 10:02:10 +0000

    feat: Add CreateAction to the header of IncidentTechniques and RapportsDisciplinaires list pages.

--- app/Filament/Resources/IncidentTechniques/Pages/ListIncidentTechniques.php
+++ app/Filament/Resources/IncidentTechniques/Pages/ListIncidentTechniques.php
@@ -3,9 +3,17 @@
 namespace App\Filament\Resources\IncidentTechniques\Pages;
 
 use App\Filament\Resources\IncidentTechniqueResource;
+use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
 
 class ListIncidentTechniques extends ListRecords
 {
     protected static string $resource = IncidentTechniqueResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            CreateAction::make(),
+        ];
+    }
 }
--- app/Filament/Resources/RapportsDisciplinaires/Pages/ListRapportsDisciplinaires.php
+++ app/Filament/Resources/RapportsDisciplinaires/Pages/ListRapportsDisciplinaires.php
@@ -3,9 +3,17 @@
 namespace App\Filament\Resources\RapportsDisciplinaires\Pages;
 
 use App\Filament\Resources\RapportDisciplinaireResource;
+use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
 
 class ListRapportsDisciplinaires extends ListRecords
 {
     protected static string $resource = RapportDisciplinaireResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            CreateAction::make(),
+        ];
+    }
 }
